c52c3c062aebe2e3dec979d53b0d062e79920d88
Roderick Fayson <rgfayson2@gmail.com>
2022-08-10 11:11:14 -0400

    sum test

---
+++ test/index.test.js
@@ -0,0 +1,14 @@
+import { sum } from '../src/index.js';
+
+it("should add 2 numbers and return the sum", () =>{
+    //Arrange
+    const a = 4, b = 5, expected = 9
+
+    //Act
+    const actual = sum(a,b);
+
+    //Assert
+    expect(actual).toBe(expected)
+
+})
+
